18b2a7315a3c1b1c3514b76de9e85bd0d119235e
Jens van Hellemondt <jens.vanhellemondt@gmail.com>
2021-03-26 20:15:22 +0100

    feat(application): App and Dashboard

---
+++ src/modules/mern-client/views/App.tsx
@@ -0,0 +1,66 @@
+import React, { ReactElement, useState } from 'react'
+import { Link, Redirect, Route, Switch, useLocation } from 'react-router-dom'
+
+import '../styles/App.sass'
+import Dashboard from './Dashboard'
+import SignIn from '../../authorization/views/SignIn'
+import PageNotFound from '../../base/views/PageNotFound'
+
+interface RouteOptions {
+	path: string
+	exact?: boolean
+	component: ReactElement
+	beforeEnter?: () => string
+}
+
+export default (): ReactElement => {
+	const [isAuthenticated] = useState(false)
+	const location = useLocation()
+	// const login = (): void => {
+	// 	isAuthenticatedSet(true)
+	// }
+	// const logout = (): void => {
+	// 	isAuthenticatedSet(false)
+	// }
+
+	const authenticationGuard = (): string => {
+		if (!isAuthenticated) return '/login'
+		return ''
+	}
+	const guestGuard = (): string => {
+		if (isAuthenticated) return '/'
+		return ''
+	}
+
+	const routes = [
+		{ path: '/', exact: true, component: <Dashboard />, beforeEnter: authenticationGuard },
+		{ path: '/login', exact: true, component: <SignIn />, beforeEnter: guestGuard },
+		{ path: '*', component: <PageNotFound /> }
+	]
+
+	return (
+		<div>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/login">Sign in</Link>
+				</li>
+			</ul>
+
+			<Switch>
+				{routes.map((route: RouteOptions) => {
+					if (route?.beforeEnter && route.beforeEnter() && route.path === location.pathname) {
+						return <Redirect key={route.path} to={{ pathname: route.beforeEnter() }} />
+					}
+					return (
+						<Route key={route.path} exact={route.exact} path={route.path}>
+							{route.component}
+						</Route>
+					)
+				})}
+			</Switch>
+		</div>
+	)
+}
